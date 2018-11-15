64006874a6b9df1c80d319cecb22ddb6a0d9c385
tinakarn <haiiplara@hotmail.com>
2018-11-15 19:09:56 +0700

    Group Test

--- libs.test.js
+++ libs.test.js
@@ -1,18 +1,17 @@
 const libs = require('./libs')
 
-test('absolute - should return a positive number if input is positive', () => {
-    // Arrange
-    const input = 1;
-    // Action
+it('absolute - should return a positive number if input is positive', () => {
+    
+});
+
+it('absolute - should return a positive number if input is negative', () => {
+    const input = -1;
     const result = libs.absolute(input);
-    // Assert
+    expect(result).toBe(1);
+});
+
+it('absolute - should return 0 if input is 0', () => {
+    const input = 0;
+    const result = libs.absolute(input);
     expect(result).toBe(input);
-});
-
-test('absolute - should return a positive number if input is negative', () => {
-
-});
-
-test('absolute - should return 0 if input is 0', () => {
-
 });
