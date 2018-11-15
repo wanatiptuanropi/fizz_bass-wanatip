034f37be24e99d3d1a0fa2c12c3ee1dcb64b92d4
tinakarn <haiiplara@hotmail.com>
2018-11-15 19:05:20 +0700

    Testing Numbers

---
+++ libs.js
@@ -0,0 +1,5 @@
+module.exports.absolute = number => {
+    if(number > 0) return number;
+    if(number < 0) return number;
+    return 0;
+}
--- libs.test.js
+++ libs.test.js
@@ -1,5 +1,18 @@
-test('My first unit test', () => {
+const libs = require('libs')
+
+test('absolute - should return a positive number if input is positive', () => {
     // Arrange
+    const input = 1;
     // Action
+    const result = libs.absolute(input);
     // Assert
+    expect(result).toBe(input0);
+});
+
+test('absolute - should return a positive number if input is negative', () => {
+
+});
+
+test('absolute - should return 0 if input is 0', () => {
+
 });
