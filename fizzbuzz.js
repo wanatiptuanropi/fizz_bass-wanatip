b1c1ba5da07eed76d476f69b312fe50185717190
work <work@lokasuns-MacBook-Air.local>
2018-11-22 12:35:09 +0700

    edit

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,6 +1,6 @@
 module.exports.fizzbuzz = number => {
     if(number %3 === 0 && number %5 === 0) return "FizzBuzz";
-    if(number %3 === 2) return 'Fizz';
+    if(number %3 === 0) return 'Fizz';
     if(number %5 === 0) return 'Buzz';
     return number;
 }
